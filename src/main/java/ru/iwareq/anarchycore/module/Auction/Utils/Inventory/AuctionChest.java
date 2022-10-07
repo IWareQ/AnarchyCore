3c469bd89388cfe00c074743e3b63a9a773faba9
IWareQ <dimonpo96@mail.ru>
2022-10-07 14:26:41 +0400

    feat(auction): Remove page items if page == 0

--- src/main/java/ru/iwareq/anarchycore/module/Auction/Utils/Inventory/AuctionChest.java
+++ src/main/java/ru/iwareq/anarchycore/module/Auction/Utils/Inventory/AuctionChest.java
@@ -30,13 +30,15 @@ public class AuctionChest extends DefaultDoubleChest {
 						"\n\n§fТорговая площадка также является\nотличным способом заработать §6Монет§7, §fпродавая\nфермерские товары§7, §fкоторые могут\nзаинтересовать других Игроков§7." +
 						"\n\n§rЧтобы выставить предмет на продажу§7,\n§fвозьмите его в руку и введите\n§6/auc §7(§6цена§7)"));
 
-		this.setItem(45, Item.get(Item.PAPER)
-				.setCustomName("§r§6Листнуть назад")
-				.setLore("\n§r§l§6• §rНажмите§7, §fчтобы перейти§7!"));
+		if (page != 1) {
+			this.setItem(45, Item.get(Item.PAPER)
+					.setCustomName("§r§6Листнуть назад")
+					.setLore("\n§r§l§6• §rНажмите§7, §fчтобы перейти§7!"));
 
-		this.setItem(53, Item.get(Item.PAPER)
-				.setCustomName("§r§6Листнуть вперед")
-				.setLore("\n§r§l§6• §rНажмите§7, §fчтобы перейти§7!"));
+			this.setItem(53, Item.get(Item.PAPER)
+					.setCustomName("§r§6Листнуть вперед")
+					.setLore("\n§r§l§6• §rНажмите§7, §fчтобы перейти§7!"));
+		}
 	}
 
 	@Override
